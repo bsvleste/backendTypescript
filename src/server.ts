513a1258ac290603d0634525d137f1b02a4ed620
Bruno <bvaleiro@gmail.com>
2022-03-11 19:24:02 -0300

    arrumadon o bacn

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,4 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' })
 })
 
-app.listen(3333,()=>{console.log("ola nmundo 2 ")})
+app.listen(3333,()=>{console.log("ola")})
